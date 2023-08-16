bbbfaef4f4cfbba7a39995ebbdd297e4f61d0348
Vicant Solutions <vicantsolutions@gmail.com>
2023-08-16 00:24:29 -0500

    first commit

---
+++ M365SATTester.ps1
@@ -0,0 +1,27 @@
+param ($UserPrincipalName)
+
+function ExecuteM365SAT
+{
+	Import-Module .\M365SAT.psd1
+	Get-M365SATReport -OutPath "C:\Out" -SkipChecks -Username $UserPrincipalName -reportType "HTML" -AllowLogging "Warning" -UseCustomModules
+	Remove-Module M365SAT
+}
+
+
+function CheckAdminPrivBeta
+{
+	# Check if script is running as Adminstrator and if not use RunAs
+	Write-Host "[...] Checking if the script is running as Administrator"
+	$IsAdmin = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+	if (-not $IsAdmin)
+	{
+		Write-Warning "[!] Program needs Administrator Rights! Trying to Elevate to Admin..."
+		Start-Process powershell -Verb runas -ArgumentList "-NoExit -c cd '$pwd'; .\M365SATTester.ps1"
+	}
+	else
+	{
+		Write-Host "[+] The script is running as Administrator..." -ForegroundColor Green
+		ExecuteM365SAT
+	}
+}
+CheckAdminPrivBeta
