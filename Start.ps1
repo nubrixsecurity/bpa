c215411b92cf4d988e86979804f19de745b12894
nubrixsecurity <142175078+nubrixsecurity@users.noreply.github.com>
2024-06-07 17:58:31 -0500

    Update Start.ps1

--- Start.ps1
+++ Start.ps1
@@ -1,20 +1,9 @@
-#RUN SCRIPT ON POWERSHELL 7.3.6
-
-$ErrorActionPreference = 'silentlycontinue'
-
-Write-Host '---- CHECKING IF GIT IS INSTALLED ----' -f CYAN
-
-#INSTALL GIT WINGET
-$gitInstalled = git --version
-if($gitInstalled -eq $null){
-    write-host "Installing Git" -Foreground yellow
-    winget install --id Git.Git -e --source winget
-}
-else{write-host "Git already installed" -Foreground green}
+#RUN SCRIPT ON POWERSHELL 5.1
 
 #INPUT VARIABLES
-$inputOrg = $(Write-Host "Input Organization Name: " -f yellow -NoNewLine; Read-Host)
-$tenantId = $(Write-Host "Input Tenant Id: " -f yellow -NoNewLine; Read-Host)
+$userPrincipalName = $(Write-Host "Enter User Name: " -f yellow -NoNewLine; Read-Host)
+$fullDomain = ($userPrincipalName -split "@")[1]
+$inputOrg = ($fullDomain -split ".c")[0]
 
 #CREATE DIRECTORY FOLDERS
 $path = 'C:\BPA'
@@ -52,7 +41,6 @@ else {
 	git clone https://github.com/asterictnl-lvdw/M365SAT $clonePathM365SAT
 }
 
-$ErrorActionPreference = 'stop'
 
 #UNBLOCK FILES
 $files = Get-ChildItem $pathDate -Recurse -File
@@ -61,49 +49,12 @@ foreach ($file in $files) {
     Unblock-File -Path $file.FullName -Confirm:$false
 }
 
-#CHECK IF MODULES EXISTS
-Write-Host '---- CHECKING MODULES ----' -f CYAN
-
-$Modules = @('Az',`
-			 'ExchangeOnlineManagement',`
-			 'Microsoft.Online.SharePoint.PowerShell',`
-			 'Microsoft.Graph',`
-			 'MicrosoftTeams',`
-			 'PoShLog')
-
-foreach($m in $Modules){
-	if($m -eq 'Az'){
-		$name = 'Az*'
-		$module = Get-Module -ListAvailable | where-Object {$_.Name -like $name} | select name
-			
-		if($module.name -ne $null){
-			write-host "Module already installed: $m" -Foreground green
-		}
-		else{
-			write-host "Installing module: $m" -Foreground yellow
-			Install-Module -Name $m -Scope CurrentUser -Force -AllowClobber
-			#Import-Module -Name $m 
-		}
-	}
-	else{
-		$module = Get-Module -Name $m -ListAvailable | select name
-			
-		if($module.name -ne $null){
-			write-host "Module already installed: $m" -Foreground green
-		}
-		else{
-			write-host "Installing module: $m" -Foreground yellow
-			Install-Module -Name $m -Scope CurrentUser -Force -AllowClobber
-			#Import-Module -Name $m 
-		}
-	}
-}
-
-#M365SAT ASSESSMENT
-Write-Host '---- RUNNING M365SAT ASSESSMENT ----' -f CYAN
-
+#COPY CUSTOM FILE TO NEW LOCATION
 $copyFile = $clonePathC1+'M365SATTester.ps1'
 Copy-Item $copyFile -Destination $clonePathM365SAT
 
-cd $clonePathM365SAT
-.\M365SAT-Start.ps1 $tenantId $outPath
+$copyFile = $clonePathC1+'M365SAT-Start.ps1'
+Copy-Item $copyFile -Destination $clonePathM365SAT
+
+Set-Location $clonePathM365SAT
+.\M365SAT-Start.ps1 $outPath $userPrincipalName 
