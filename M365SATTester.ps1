2bd79fe3122c6a899a2b89599985a52c209c6736
nubrixsecurity <142175078+nubrixsecurity@users.noreply.github.com>
2024-06-07 22:03:10 -0500

    Update M365SATTester.ps1

--- M365SATTester.ps1
+++ M365SATTester.ps1
@@ -16,7 +16,7 @@ function ExecuteM365SAT
 		-BenchmarkVersion LATEST `
 		-LicenseMode All `
 		-LicenseLevel All `
-		-EnvironmentType AZURE,M365 `
+		-EnvironmentType M365 `
 		-LocalMode
 
 	Remove-Module M365SAT
